f914ae4af90a3340c936591d629f0b98dc29d328
WhiteShadow <aayushkuinkel41n1@gmail.com>
2025-07-08 08:43:46 +0545

    loader color change to dark

--- Frontend/src/component/Common/Dashboard/LoadingComponent.jsx
+++ Frontend/src/component/Common/Dashboard/LoadingComponent.jsx
@@ -5,7 +5,7 @@ const LoadingComponent = () => {
     <>
     <div className="d-flex justify-content-center align-items-center vh-100 bg-white">
       <div className="text-center">
-        <div className="spinner-border text-primary" role="status" style={{ width: '4rem', height: '4rem' }}>
+        <div className="spinner-border text-dark" role="status" style={{ width: '4rem', height: '4rem' }}>
           <span className="visually-hidden">Loading...</span>
         </div>
         <p className="mt-3 fs-5 fw-semibold text-secondary">Loading...</p>
