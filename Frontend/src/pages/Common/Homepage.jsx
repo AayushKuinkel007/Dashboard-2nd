37079064343894b505509fa4cf71eb294756beb2
WhiteShadow <aayushkuinkel41n1@gmail.com>
2025-07-08 08:13:57 +0545

    homepage products and css update

--- Frontend/src/pages/Common/Homepage.jsx
+++ Frontend/src/pages/Common/Homepage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import LoadingComponent from "../../component/Common/Dashboard/LoadingComponent";
 import Navbar from "../../component/Common/Navbar";
 import { useSelector } from "react-redux";
-
+import '../../css/Homepage.css'
 const Homepage = () => {
   const [loading, setLoading] = useState(true);
 
@@ -24,16 +24,30 @@ const Homepage = () => {
             <div className="row justify-content-center">
               {cardData &&
                 cardData.map((product, index) => (
-                  <div className="col-md-4 d-flex justify-content-center mb-4" key={index}>
-                    <div className="card" style={{ width: "18rem" }}>
-                      <img src={product.img} className="card-img-top" alt="..." />
+                  <div
+                    className="col-md-4 d-flex justify-content-center mb-4"
+                    key={index}
+                  >
+                    <div
+                      className="card product-card p-3"
+                      style={{ width: "18rem" }}
+                    >
+                      <img
+                        src={product.img}
+                        className="card-img-top"
+                        alt="..."
+                      />
                       <div className="card-body text-center">
                         <h5 className="card-title">{product.name}</h5>
+                        <p className="card-text">
+                          Price: <strong>{product.price}</strong>
+                        </p>
                         <p className="card-text">
                           OnStock: <strong>{product.onstock}</strong>
                         </p>
                       </div>
                       <div className="p-3 d-flex justify-content-center">
+
                         <button className="btn btn-dark">Add To Cart</button>
                       </div>
                     </div>
--- Frontend/src/slices/common/productSlice.jsx
+++ Frontend/src/slices/common/productSlice.jsx
@@ -1,85 +1,145 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const productSlice = createSlice({
-    name:'products',
-    initialState:{
-        products:[
-            {
-                name:"Laptop",
-                price:'5000',
-                onstock:'yes',
-                img:'https://i5.walmartimages.com/seo/HP-15-6-FHD-Laptop-AMD-Ryzen-5-7520U-8GB-RAM-256GB-SSD-Pale-Rose-Gold-Windows-11-Home-15-fc0039wm_b1326872-e582-4ad9-b068-c74f24128204.29680ead27a949c3afd39c55a9c3b1d6.jpeg'
-            },
-                        {
-                name:"Computer",
-                price:'5000',
-                onstock:'yes',
-                img:'https://i5.walmartimages.com/seo/HP-15-6-FHD-Laptop-AMD-Ryzen-5-7520U-8GB-RAM-256GB-SSD-Pale-Rose-Gold-Windows-11-Home-15-fc0039wm_b1326872-e582-4ad9-b068-c74f24128204.29680ead27a949c3afd39c55a9c3b1d6.jpeg'
-            },
-                        {
-                name:"PC",
-                price:'5000',
-                onstock:'yes',
-                img:'https://i5.walmartimages.com/seo/HP-15-6-FHD-Laptop-AMD-Ryzen-5-7520U-8GB-RAM-256GB-SSD-Pale-Rose-Gold-Windows-11-Home-15-fc0039wm_b1326872-e582-4ad9-b068-c74f24128204.29680ead27a949c3afd39c55a9c3b1d6.jpeg'
-            },
-                        {
-                name:"Laptop",
-                price:'5000',
-                onstock:'yes',
-                img:'https://i5.walmartimages.com/seo/HP-15-6-FHD-Laptop-AMD-Ryzen-5-7520U-8GB-RAM-256GB-SSD-Pale-Rose-Gold-Windows-11-Home-15-fc0039wm_b1326872-e582-4ad9-b068-c74f24128204.29680ead27a949c3afd39c55a9c3b1d6.jpeg'
-            },
-                        {
-                name:"Computer",
-                price:'5000',
-                onstock:'yes',
-                img:'https://i5.walmartimages.com/seo/HP-15-6-FHD-Laptop-AMD-Ryzen-5-7520U-8GB-RAM-256GB-SSD-Pale-Rose-Gold-Windows-11-Home-15-fc0039wm_b1326872-e582-4ad9-b068-c74f24128204.29680ead27a949c3afd39c55a9c3b1d6.jpeg'
-            },
-                        {
-                name:"PC",
-                price:'5000',
-                onstock:'yes',
-                img:'https://i5.walmartimages.com/seo/HP-15-6-FHD-Laptop-AMD-Ryzen-5-7520U-8GB-RAM-256GB-SSD-Pale-Rose-Gold-Windows-11-Home-15-fc0039wm_b1326872-e582-4ad9-b068-c74f24128204.29680ead27a949c3afd39c55a9c3b1d6.jpeg'
-            },
-                        {
-                name:"Laptop",
-                price:'5000',
-                onstock:'yes',
-                img:'https://i5.walmartimages.com/seo/HP-15-6-FHD-Laptop-AMD-Ryzen-5-7520U-8GB-RAM-256GB-SSD-Pale-Rose-Gold-Windows-11-Home-15-fc0039wm_b1326872-e582-4ad9-b068-c74f24128204.29680ead27a949c3afd39c55a9c3b1d6.jpeg'
-            },
-                        {
-                name:"Computer",
-                price:'5000',
-                onstock:'yes',
-                img:'https://i5.walmartimages.com/seo/HP-15-6-FHD-Laptop-AMD-Ryzen-5-7520U-8GB-RAM-256GB-SSD-Pale-Rose-Gold-Windows-11-Home-15-fc0039wm_b1326872-e582-4ad9-b068-c74f24128204.29680ead27a949c3afd39c55a9c3b1d6.jpeg'
-            },
-                        {
-                name:"PC",
-                price:'5000',
-                onstock:'yes',
-                img:'https://i5.walmartimages.com/seo/HP-15-6-FHD-Laptop-AMD-Ryzen-5-7520U-8GB-RAM-256GB-SSD-Pale-Rose-Gold-Windows-11-Home-15-fc0039wm_b1326872-e582-4ad9-b068-c74f24128204.29680ead27a949c3afd39c55a9c3b1d6.jpeg'
-            },
-                        {
-                name:"Laptop",
-                price:'5000',
-                onstock:'yes',
-                img:'https://i5.walmartimages.com/seo/HP-15-6-FHD-Laptop-AMD-Ryzen-5-7520U-8GB-RAM-256GB-SSD-Pale-Rose-Gold-Windows-11-Home-15-fc0039wm_b1326872-e582-4ad9-b068-c74f24128204.29680ead27a949c3afd39c55a9c3b1d6.jpeg'
-            },
-                        {
-                name:"Computer",
-                price:'5000',
-                onstock:'yes',
-                img:'https://i5.walmartimages.com/seo/HP-15-6-FHD-Laptop-AMD-Ryzen-5-7520U-8GB-RAM-256GB-SSD-Pale-Rose-Gold-Windows-11-Home-15-fc0039wm_b1326872-e582-4ad9-b068-c74f24128204.29680ead27a949c3afd39c55a9c3b1d6.jpeg'
-            },
-                        {
-                name:"PC",
-                price:'5000',
-                onstock:'yes',
-                img:'https://i5.walmartimages.com/seo/HP-15-6-FHD-Laptop-AMD-Ryzen-5-7520U-8GB-RAM-256GB-SSD-Pale-Rose-Gold-Windows-11-Home-15-fc0039wm_b1326872-e582-4ad9-b068-c74f24128204.29680ead27a949c3afd39c55a9c3b1d6.jpeg'
-            },
-        ]
-    }
-}
-
-)
-export default productSlice.reducer
+  name: "products",
+  initialState: {
+    products: [
+      {
+        id: 1,
+        name: "Asus-ROG XG49VQ",
+        price: "$1,169",
+        onstock: "Yes",
+        img: "https://dlcdnwebimgs.asus.com/gain/EA5F4487-87F4-47AC-B06F-EC1C95C0E59E/w717/h525",
+        specifications: "this is a product",
+        product: "monitor",
+      },
+      {
+        id: 2,
+        name: "MSI G255F",
+        price: "$195",
+        onstock: "Out of Stock",
+        img: "https://mudita.com.np/media/catalog/product/cache/036aa47742df3225beb764097d8b4fe8/m/s/msi-g255f-monitor-price-in-nepal.webp",
+        specifications: "this is a product",
+        product: "monitor",
+      },
+      {
+        id: 3,
+        name: "Sansui es-27x3 ",
+        price: "$90",
+        onstock: "Yes",
+        img: "https://m.media-amazon.com/images/I/71KYyiUlMPL.jpg",
+        specifications: "this is a product",
+        product: "monitor",
+      },
+      {
+        id: 4,
+        name: "Asus-ROG Strix Scar 15",
+        price: "$1950",
+        onstock: "Yes",
+        img: "https://mudita.com.np/media/catalog/product/cache/5f4a658faeee583187031a67361d4d52/a/s/asus-rog-strix-scar-15-price-in-nepal.webp",
+        specifications: "this is a product",
+        product: "laptop",
+      },
+      {
+        id: 5,
+        name: "Acer-Nitro V 15",
+        price: "$915",
+        onstock: "Yes",
+        img: "https://mudita.com.np/media/catalog/product/cache/5f4a658faeee583187031a67361d4d52/a/c/acer-nitro-v-15-i5-4060-price-in-nepal.webp",
+        specifications: "this is a product",
+        product: "laptop",
+      },
+      {
+        id: 6,
+        name: "Lenovo Legion 5",
+        price: "$1220",
+        onstock: "1 left",
+        img: "https://mudita.com.np/media/catalog/product/cache/5f4a658faeee583187031a67361d4d52/l/e/lenovo-legion-16irx9-price-in-nepal_1.webp",
+        specifications: "this is a product",
+        product: "laptop",
+      },
+      {
+        id: 7,
+        name: "Fantech WGC1 Venom",
+        price: "$15",
+        onstock: "2 Left",
+        img: "https://fantech.ph/wp-content/uploads/2019/11/fantech-wgc1.jpg",
+        specifications: "this is a product",
+        product: "mouse",
+      },
+      {
+        id: 8,
+        name: "Red Magic Nubia",
+        price: "$20",
+        onstock: "Yes",
+        img: "https://cdn.gadgetbytenepal.com/wp-content/uploads/2022/07/Nubia-RedMagic-Gaming-Keyboard.jpg",
+        specifications: "this is a product",
+        product: "mouse",
+      },
+      {
+        id: 9,
+        name: "Red Dragon Dagger-2",
+        price: "$25",
+        onstock: "No",
+        img: "https://redragonadria.com/wp-content/uploads/2021/03/M715-1%E2%95%95%E2%96%92%E2%96%92%E2%95%9B.jpg",
+        specifications: "this is a product",
+        product: "mouse",
+      },
+      {
+        id: 10,
+        name: "Ajazz AK820 MAX",
+        price: "$90",
+        onstock: "no",
+        img: "https://backseatgaming.b-cdn.net/wp-content/uploads/2024/08/34.webp",
+        specifications: "this is a product",
+        product: "keyboard",
+      },
+      {
+        id: 11,
+        name: "Fantech Maxfit-61 Frost",
+        price: "$75",
+        onstock: "yes",
+        img: "https://gadgethousenepal.com/wp-content/uploads/2022/09/fc8d0299.png",
+        specifications: "this is a product",
+        product: "keyboard",
+      },
+      {
+        id: 12,
+        name: "RedDragon K552",
+        price: "$60",
+        onstock: "no",
+        img: "https://m.media-amazon.com/images/I/71FSIp+tDNL._AC_UF894,1000_QL80_.jpg",
+        specifications: "this is a product",
+        product: "keyboard",
+      },
+      {
+        id: 13,
+        name: "Dragon Theme Deskmat",
+        price: "$10",
+        onstock: "yes",
+        img: "https://www.drakvane.com/cdn/shop/files/preview_images/Sb97ae8bc45744ed59cd78e1ef56534f7q_600x600_crop_center.webp?v=1735600787",
+        specifications: "this is a product",
+        product: "accessories",
+      },
+      {
+        id: 14,
+        name: "Coiled C-Cable",
+        price: "$5",
+        onstock: "no",
+        img: "https://cdn.cablemod.com/wp-content/uploads/2021/11/Midnight-Black_Classic_Amazon-1.jpg",
+        specifications: "this is a product",
+        product: "accessories",
+      },
+      {
+        id: 15,
+        name: "Keycaps remover",
+        price: "$5",
+        onstock: "no",
+        img: "https://img.fruugo.com/product/6/48/223378486_max.jpg",
+        specifications: "this is a product",
+        product: "accessories",
+      },
+    ],
+  },
+});
+export default productSlice.reducer;
